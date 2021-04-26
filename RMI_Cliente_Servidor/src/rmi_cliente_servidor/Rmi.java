1fcd19d01d39ccc7ebe2bbc8a407952a6d052473
Miguel Angel <leonaguirrem3@gmail.com>
2021-04-26 18:47:03 -0500

    Commit: Se modificó el archivo Rmi

--- RMI_Cliente_Servidor/src/rmi_cliente_servidor/Rmi.java
+++ RMI_Cliente_Servidor/src/rmi_cliente_servidor/Rmi.java
@@ -1,9 +1,17 @@
 package rmi_cliente_servidor;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  *
  * @author miguel
  */
-public class Rmi {
+public class Rmi extends UnicastRemoteObject implements Suma{
+
+    @Override
+    public String suma(String a, String b) throws RemoteException {
+        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+    }
     
 }
